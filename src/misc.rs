a7927e35bd3a21ed40f6ec83603c07f600d479b0
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-10-16 15:31:54 -0400

    Fix invalid DER-encoding of Length

--- src/misc.rs
+++ src/misc.rs
@@ -98,7 +98,7 @@ impl Length {
 	pub fn deserialized(mut reader: impl Read) -> Result<usize, SerdeAsn1DerError> {
 		// Deserialize length
 		Ok(match reader.read_one()? {
-			n @ 128...255 => {
+			n @ 128..=255 => {
 				// Deserialize the amount of length bytes
 				let len = n as usize & 127;
 				if len > USIZE_LEN { Err(SerdeAsn1DerError::UnsupportedValue)? }
@@ -115,26 +115,57 @@ impl Length {
 	/// Serializes `len` to `writer`
 	pub fn serialize(len: usize, mut writer: impl Write) -> Result<usize, SerdeAsn1DerError> {
 		// Determine the serialized length
-		let to_write = match len {
-			0...127 => 1,
-			len => 1 + USIZE_LEN - (len.leading_zeros() / 8) as usize
-		};
-		
-		// Serialize and write the number
-		match to_write {
-			1 => {
-				writer.write_one(len as u8)?;
-			},
+		let written = match len {
+			0..=127 => writer.write_one(len as u8)?,
 			_ => {
-				// Write length byte
-				writer.write_one(to_write as u8 | 0x80)?;
-				
-				// Write number
+				let to_write = USIZE_LEN - (len.leading_zeros() / 8) as usize;
+				// Write number of bytes used to encode length
+				let mut written = writer.write_one(to_write as u8 | 0x80)?;
+
+				// Write length
 				let mut buf = [0; USIZE_LEN];
 				buf.copy_from_slice(&len.to_be_bytes());
-				writer.write_exact(&buf[to_write - 1 ..])?;
-			}
-		}
-		Ok(to_write)
+				written += writer.write_exact(&buf[USIZE_LEN - to_write..])?;
+
+				written
+			},
+		};
+
+		Ok(written)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn asn1_short_form_length() {
+		let mut writer: Vec<u8> = Vec::new();
+		let written = Length::serialize(10, &mut writer).expect("serialization failed");
+		assert_eq!(written, 1);
+		assert_eq!(writer.len(), 1);
+		assert_eq!(writer[0], 10);
+	}
+
+	#[test]
+	fn asn1_long_form_length_1_byte() {
+		let mut writer: Vec<u8> = Vec::new();
+		let written = Length::serialize(129, &mut writer).expect("serialization failed");
+		assert_eq!(written, 2);
+		assert_eq!(writer.len(), 2);
+		assert_eq!(writer[0], 0x81);
+		assert_eq!(writer[1], 0x81);
+	}
+
+	#[test]
+	fn asn1_long_form_length_2_bytes() {
+		let mut writer: Vec<u8> = Vec::new();
+		let written = Length::serialize(290, &mut writer).expect("serialization failed");
+		assert_eq!(written, 3);
+		assert_eq!(writer.len(), 3);
+		assert_eq!(writer[0], 0x82);
+		assert_eq!(writer[1], 0x01);
+		assert_eq!(writer[2], 0x22);
 	}
 }
